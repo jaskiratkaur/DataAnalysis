d3aac8604cf081a972ea1fa3ddf250fc9ecb805c
jaskiratkaur <40700334+jaskiratkaur@users.noreply.github.com>
2020-05-29 21:58:46 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,12 @@
+t <- read.table("C:/Users/manjot/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+t <- t[complete.cases(t),]
+dateTime <- paste(t$Date, t$Time)
+dateTime <- setNames(dateTime, "DateTime")
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+t <- cbind(dateTime, t)
+t$dateTime <- as.POSIXct(dateTime)
+png("plot1.png", height = 480, width = 480)
+hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+dev.off()
